75930e46a0af8563aa62b2145a777ae097c09cb3
ThiagoBenz <98067252+ThiagoBenz@users.noreply.github.com>
2022-01-20 00:34:29 -0300

    Adicionar DAO pagamento

---
+++ AssistenciaTecnica/src/core/DAOPagamento.java
@@ -0,0 +1,37 @@
+package core;
+
+import java.util.ArrayList;
+
+public class DAOPagamento implements IDataAccessObject<Pagamento> {
+
+    @Override
+    public void create(Pagamento obj) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void delete(int id) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void update(Pagamento obj) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Pagamento read(int id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ArrayList<Pagamento> list() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
