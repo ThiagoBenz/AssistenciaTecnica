8c033a5135c0164b869959fd344fcfe25c923f99
ThiagoBenz <98067252+ThiagoBenz@users.noreply.github.com>
2022-01-20 00:32:38 -0300

    Adicionar DAO de reparo

---
+++ AssistenciaTecnica/src/core/DAOReparo.java
@@ -0,0 +1,37 @@
+package core;
+
+import java.util.ArrayList;
+
+public class DAOReparo implements IDataAccessObject<Reparo> {
+
+    @Override
+    public void create(Reparo obj) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void delete(int id) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void update(Reparo obj) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Reparo read(int id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ArrayList<Reparo> list() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
