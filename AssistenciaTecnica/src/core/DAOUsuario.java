7450a2188a35db3a1671772bbde60fb714fa82d2
ThiagoBenz <98067252+ThiagoBenz@users.noreply.github.com>
2022-01-20 00:29:27 -0300

    Adicionar DAO de usuario

---
+++ AssistenciaTecnica/src/core/DAOUsuario.java
@@ -0,0 +1,38 @@
+package core;
+
+import java.util.ArrayList;
+
+public class DAOUsuario implements IDataAccessObject<Usuario> {
+
+    @Override
+    public void create(Usuario obj) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void delete(int id) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void update(Usuario obj) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Usuario read(int id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public ArrayList<Usuario> list() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    
+}
