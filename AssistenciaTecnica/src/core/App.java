1c2d7911d840102bff7ae4ca44c35a5f7e551cbc
ThiagoBenz <98067252+ThiagoBenz@users.noreply.github.com>
2022-01-19 23:50:06 -0300

    Adicionar classe App

---
+++ AssistenciaTecnica/src/core/App.java
@@ -0,0 +1,22 @@
+package core;
+
+public class App {
+
+    public int menuPrincipal() {
+		System.out.println("MENU PRINCIPAL" );
+		System.out.println("\n");
+		System.out.println("1- ");
+
+		return 0;
+	}
+
+	public int appRuntime() {
+		// Carregar informações do banco de dados
+
+		// Iniciar programa
+		menuPrincipal();
+		
+		return 0;
+	}
+    
+}
--- AssistenciaTecnica/src/core/Main.java
+++ AssistenciaTecnica/src/core/Main.java
@@ -1,10 +1,12 @@
 package core;
 
 public class Main {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!!!");
-	}
 
+		System.out.println("Hello World!!!");
+
+		App app = new App();
+		app.appRuntime();
+	}
 }
